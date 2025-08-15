ebfd8955307d9efbf886d0f0f024fb325c05f7de
Da-progress <murder8910@mail.ru>
2025-08-15 13:02:51 +0300

    project and pdf

---
+++ zwrot2.m
@@ -0,0 +1,297 @@
+LAThj1=LATH1 + Kh2*kdt2*kt22*kt2*kdi2*do2*cos(b1*pi/180)/60
+LONGhj1=LongH1+ Kh2*kdt2*kt22*kt2*kdi2*do2*sin(b1*pi/180)/60     
+if p2+1<=length(unique(Turning2)) && Turn1>4 b2=Turning2(1,p2+1)
+else b2=Emp
+end
+LAThj2=LAThj1 + Kh2*kdt2*kt22*kt2*kdi2*do2*cos(b2*pi/180)/60
+LONGhj2=LONGhj1 + Kh2*kdt2*kt22*kt2*kdi2*do2*sin(b2*pi/180)/60     
+if p2+2<=length(unique(Turning2)) b3=Turning2(1,p2+2)
+else b3=Emp
+end
+LAThj3=LAThj2 + kt2*kdi2*do2*cos(b3*pi/180)/60
+LONGhj3=LONGhj2 + kt2*kdi2*do2*sin(b3*pi/180)/60     
+if p2+3<=length(unique(Turning2)) b4=Turning2(1,p2+3) 
+else b4=Emp; 
+end
+LAThj4=LAThj3 + kt2*kdi2*do2*cos(b4*pi/180)/60;
+LONGhj4=LONGhj3 + kt2*kdi2*do2*sin(b4*pi/180)/60     
+if  p2+4<=length(unique(Turning2)) b5=Turning2(1,p2+4) 
+else b5=Emp 
+end
+LAThj5=LAThj4 + kt2*kdi2*do2*cos(b5*pi/180)/60
+LONGhj5=LONGhj4 + kt2*kdi2*do2*sin(b5*pi/180)/60     
+if p2+5<=length(unique(Turning2)) b6=Turning2(1,p2+5) 
+else b6=Emp 
+end
+LAThj6=LAThj5 + kt2*kdi2*do2*cos(b6*pi/180)/60
+LONGhj6=LONGhj5 + kt2*kdi2*do2*sin(b6*pi/180)/60     
+if p2+6<=length(unique(Turning2)) b7=Turning2(1,p2+6) 
+else b7=Emp 
+end
+LAThj7=LAThj6 + kt2*kdi2*do2*cos(b7*pi/180)/60
+LONGhj7=LONGhj6 + kt2*kdi2*do2*sin(b7*pi/180)/60     
+if p2+7<=length(unique(Turning2)) b8=Turning2(1,p2+7) 
+else b8=Emp 
+end
+LAThj8=LAThj7 + kt2*kdi2*do2*cos(b8*pi/180)/60
+LONGhj8=LONGhj7 + kt2*kdi2*do2*sin(b8*pi/180)/60     
+if  p2+8<=length(unique(Turning2)) b9=Turning2(1,p2+8) 
+else b9=Emp 
+end
+LAThj9=LAThj8 + kt2*kdi2*do2*cos(b9*pi/180)/60
+LONGhj9=LONGhj8 + kt2*kdi2*do2*sin(b9*pi/180)/60     
+if p2+9<=length(unique(Turning2)) b10=Turning2(1,p2+9) 
+else b10=Emp 
+end
+LAThj10=LAThj9 + kt2*kdi2*do2*cos(b10*pi/180)/60
+LONGhj10=LONGhj9 + kt2*kdi2*do2*sin(b10*pi/180)/60     
+if p2+10<=length(unique(Turning2)) b11=Turning2(1,p2+10) 
+else b11=Emp 
+end
+LAThj11=LAThj10 + kt2*kdi2*do2*cos(b11*pi/180)/60
+LONGhj11=LONGhj10 + kt2*kdi2*do2*sin(b11*pi/180)/60     
+if p2+11<=length(unique(Turning2)) b12=Turning2(1,p2+11) 
+else b12=Emp 
+end
+LAThj12=LAThj11 + kt2*kdi2*do2*cos(b12*pi/180)/60
+LONGhj12=LONGhj11 + kt2*kdi2*do2*sin(b12*pi/180)/60     
+if p2+12<=length(unique(Turning2)) b13=Turning2(1,p2+12) 
+else b13=Emp 
+end
+LAThj13=LAThj12 + kt2*kdi2*do2*cos(b13*pi/180)/60
+LONGhj13=LONGhj12 + kt2*kdi2*do2*sin(b13*pi/180)/60     
+if p2+13<=length(unique(Turning2)) b14=Turning2(1,p2+13) 
+else b14=Emp 
+end
+LAThj14=LAThj13 + kt2*kdi2*do2*cos(b14*pi/180)/60
+LONGhj14=LONGhj13 + kt2*kdi2*do2*sin(b14*pi/180)/60     
+if p2+14<=length(unique(Turning2)) b15=Turning2(1,p2+14) 
+else b15=Emp 
+end
+LAThj15=LAThj14 + kt2*kdi2*do2*cos(b15*pi/180)/60
+LONGhj15=LONGhj14 + kt2*kdi2*do2*sin(b15*pi/180)/60     
+if p2+15<=length(unique(Turning2)) b16=Turning2(1,p2+15) 
+else b16=Emp 
+end
+LAThj16=LAThj15 + kt2*kdi2*do2*cos(b16*pi/180)/60
+LONGhj16=LONGhj15 + kt2*kdi2*do2*sin(b16*pi/180)/60     
+if p2+16<=length(unique(Turning2)) b17=Turning2(1,p2+16) 
+else b17=Emp 
+end
+LAThj17=LAThj16 + kt2*kdi2*do2*cos(b17*pi/180)/60
+LONGhj17=LONGhj16 + kt2*kdi2*do2*sin(b17*pi/180)/60     
+if p2+17<=length(unique(Turning2))  b18=Turning2(1,p2+17) 
+else b18=Emp 
+end
+LAThj18=LAThj17 + kt2*kdi2*do2*cos(b18*pi/180)/60
+LONGhj18=LONGhj17 + kt2*kdi2*do2*sin(b18*pi/180)/60     
+if p2+18<=length(unique(Turning2)) b19=Turning2(1,p2+18) 
+else b19=Emp 
+end
+LAThj19=LAThj18 + kt2*kdi2*do2*cos(b19*pi/180)/60
+LONGhj19=LONGhj18 + kt2*kdi2*do2*sin(b19*pi/180)/60     
+if p2+19<=length(unique(Turning2)) b20=Turning2(1,p2+19) 
+else b20=Emp 
+end
+LAThj20=LAThj19 + kt2*kdi2*do2*cos(b20*pi/180)/60
+LONGhj20=LONGhj19 + kt2*kdi2*do2*sin(b20*pi/180)/60     
+if p2+20<=length(unique(Turning2)) b21=Turning2(1,p2+20) 
+else b21=Emp 
+end
+LAThj21=LAThj20 + kt2*kdi2*do2*cos(b21*pi/180)/60
+LONGhj21=LONGhj20 + kt2*kdi2*do2*sin(b21*pi/180)/60     
+if p2+21<=length(unique(Turning2)) b22=Turning2(1,p2+21) 
+else b22=Emp 
+end
+LAThj22=LAThj21 + kt2*kdi2*do2*cos(b22*pi/180)/60
+LONGhj22=LONGhj21 + kt2*kdi2*do2*sin(b22*pi/180)/60     
+if p2+22<=length(unique(Turning2)) b23=Turning2(1,p2+22) 
+else b23=Emp 
+end
+LAThj23=LAThj22 + kt2*kdi2*do2*cos(b23*pi/180)/60
+LONGhj23=LONGhj22 + kt2*kdi2*do2*sin(b23*pi/180)/60     
+if p2+23<=length(unique(Turning2)) b24=Turning2(1,p2+23) 
+else b24=Emp 
+end
+LAThj24=LAThj23 + kt2*kdi2*do2*cos(b24*pi/180)/60
+LONGhj24=LONGhj23 + kt2*kdi2*do2*sin(b24*pi/180)/60     
+if p2+24<=length(unique(Turning2)) b25=Turning2(1,p2+24) 
+else b25=Emp 
+end 
+LAThj25=LAThj24 + kt2*kdi2*do2*cos(b25*pi/180)/60
+LONGhj25=LONGhj24 + kt2*kdi2*do2*sin(b25*pi/180)/60     
+if p2+25<=length(unique(Turning2)) b26=Turning2(1,p2+25) 
+else b26=Emp 
+end 
+LAThj26=LAThj25 + kt2*kdi2*do2*cos(b26*pi/180)/60
+LONGhj26=LONGhj25 + kt2*kdi2*do2*sin(b26*pi/180)/60     
+if p2+26<=length(unique(Turning2)) b27=Turning2(1,p2+26) 
+else b27=Emp 
+end 
+LAThj27=LAThj26 + kt2*kdi2*do2*cos(b27*pi/180)/60
+LONGhj27=LONGhj26 + kt2*kdi2*do2*sin(b27*pi/180)/60     
+if p2+27<=length(unique(Turning2)) b28=Turning2(1,p2+27) 
+else b28=Emp 
+end 
+LAThj28=LAThj27 + kt2*kdi2*do2*cos(b28*pi/180)/60
+LONGhj28=LONGhj27 + kt2*kdi2*do2*sin(b28*pi/180)/60     
+if p2+28<=length(unique(Turning2)) b29=Turning2(1,p2+28) 
+else b29=Emp 
+end 
+LAThj29=LAThj28 + kt2*kdi2*do2*cos(b29*pi/180)/60
+LONGhj29=LONGhj28 + kt2*kdi2*do2*sin(b29*pi/180)/60     
+if p2+29<=length(unique(Turning2)) b30=Turning2(1,p2+29) 
+else b30=Emp 
+end 
+LAThj30=LAThj29 + kt2*kdi2*do2*cos(b30*pi/180)/60
+LONGhj30=LONGhj29 + kt2*kdi2*do2*sin(b30*pi/180)/60     
+if p2+30<=length(unique(Turning2)) b31=Turning2(1,p2+30) 
+else b31=Emp 
+end 
+LAThj31=LAThj30 + kt2*kdi2*do2*cos(b31*pi/180)/60
+LONGhj31=LONGhj30 + kt2*kdi2*do2*sin(b31*pi/180)/60     
+if p2+31<=length(unique(Turning2)) b32=Turning2(1,p2+31) 
+else b32=Emp 
+end 
+LAThj32=LAThj31 + kt2*kdi2*do2*cos(b32*pi/180)/60
+LONGhj32=LONGhj31 + kt2*kdi2*do2*sin(b32*pi/180)/60     
+if p2+32<=length(unique(Turning2)) b33=Turning2(1,p2+32)
+else b33=Emp
+end
+LAThj33=LAThj32 + kt2*kdi2*do2*cos(b33*pi/180)/60
+LONGhj33=LONGhj32 + kt2*kdi2*do2*sin(b33*pi/180)/60     
+if p2+33<=length(unique(Turning2)) b34=Turning2(1,p2+33) 
+else b34=Emp 
+end
+LAThj34=LAThj33 + kt2*kdi2*do2*cos(b34*pi/180)/60
+LONGhj34=LONGhj33 + kt2*kdi2*do2*sin(b34*pi/180)/60     
+if p2+34<=length(unique(Turning2)) b35=Turning2(1,p2+34) 
+else b35=Emp 
+end
+LAThj35=LAThj34 + kt2*kdi2*do2*cos(b35*pi/180)/60
+LONGhj35=LONGhj34 + kt2*kdi2*do2*sin(b35*pi/180)/60     
+if p2+35<=length(unique(Turning2)) b36=Turning2(1,p2+35) 
+else b36=Emp 
+end
+LAThj36=LAThj35 + kt2*kdi2*do2*cos(b36*pi/180)/60
+LONGhj36=LONGhj35 + kt2*kdi2*do2*sin(b36*pi/180)/60     
+if p2+36<=length(unique(Turning2)) b37=Turning2(1,p2+36) 
+else b37=Emp 
+end
+LAThj37=LAThj36 + kt2*kdi2*do2*cos(b37*pi/180)/60
+LONGhj37=LONGhj36 + kt2*kdi2*do2*sin(b37*pi/180)/60     
+if p2+37<=length(unique(Turning2)) b38=Turning2(1,p2+37) 
+else b38=Emp 
+end
+LAThj38=LAThj37 + kt2*kdi2*do2*cos(b38*pi/180)/60
+LONGhj38=LONGhj37 + kt2*kdi2*do2*sin(b38*pi/180)/60     
+if p2+38<=length(unique(Turning2)) b39=Turning2(1,p2+38) 
+else b39=Emp 
+end
+LAThj39=LAThj38 + kt2*kdi2*do2*cos(b39*pi/180)/60
+LONGhj39=LONGhj38 + kt2*kdi2*do2*sin(b39*pi/180)/60     
+if p2+39<=length(unique(Turning2)) b40=Turning2(1,p2+39) 
+else b40=Emp 
+end
+LAThj40=LAThj39 + kt2*kdi2*do2*cos(b40*pi/180)/60
+LONGhj40=LONGhj39 + kt2*kdi2*do2*sin(b40*pi/180)/60     
+if p2+40<=length(unique(Turning2)) b41=Turning2(1,p2+40) 
+else b41=Emp 
+end
+LAThj41=LAThj40 + kt2*kdi2*do2*cos(b41*pi/180)/60
+LONGhj41=LONGhj40 + kt2*kdi2*do2*sin(b41*pi/180)/60     
+if p2+41<=length(unique(Turning2)) b42=Turning2(1,p2+41) 
+else b42=Emp 
+end
+LAThj42=LAThj41 + kt2*kdi2*do2*cos(b42*pi/180)/60
+LONGhj42=LONGhj41 + kt2*kdi2*do2*sin(b42*pi/180)/60     
+if p2+42<=length(unique(Turning2)) b43=Turning2(1,p2+42) 
+else b43=Emp 
+end
+LAThj43=LAThj42 + kt2*kdi2*do2*cos(b43*pi/180)/60
+LONGhj43=LONGhj42 + kt2*kdi2*do2*sin(b43*pi/180)/60     
+if p2+43<=length(unique(Turning2)) b44=Turning2(1,p2+43) 
+else b44=Emp 
+end
+LAThj44=LAThj43 + kt2*kdi2*do2*cos(b44*pi/180)/60
+LONGhj44=LONGhj43 + kt2*kdi2*do2*sin(b44*pi/180)/60     
+if p2+44<=length(unique(Turning2)) b45=Turning2(1,p2+44) 
+else b45=Emp 
+end
+LAThj45=LAThj44 + kt2*kdi2*do2*cos(b45*pi/180)/60
+LONGhj45=LONGhj44 + kt2*kdi2*do2*sin(b45*pi/180)/60     
+if p2+45<=length(unique(Turning2)) b46=Turning2(1,p2+45) 
+else b46=Emp 
+end
+LAThj46=LAThj45 + kt2*kdi2*do2*cos(b46*pi/180)/60
+LONGhj46=LONGhj45 + kt2*kdi2*do2*sin(b46*pi/180)/60     
+if p2+46<=length(unique(Turning2)) b47=Turning2(1,p2+46) 
+else b47=Emp 
+end
+LAThj47=LAThj46 + kt2*kdi2*do2*cos(b47*pi/180)/60
+LONGhj47=LONGhj46 + kt2*kdi2*do2*sin(b47*pi/180)/60     
+if p2+47<=length(unique(Turning2)) b48=Turning2(1,p2+47) 
+else b48=Emp 
+end
+LAThj48=LAThj47 + kt2*kdi2*do2*cos(b48*pi/180)/60
+LONGhj48=LONGhj47 + kt2*kdi2*do2*sin(b48*pi/180)/60     
+if p2+48<=length(unique(Turning2)) b49=Turning2(1,p2+48) 
+else b49=Emp 
+end
+LAThj49=LAThj48 + kt2*kdi2*do2*cos(b49*pi/180)/60
+LONGhj49=LONGhj48 + kt2*kdi2*do2*sin(b49*pi/180)/60     
+if p2+49<=length(unique(Turning2)) b50=Turning2(1,p2+49) 
+else b50=Emp 
+end
+LAThj50=LAThj49 + kt2*kdi2*do2*cos(b50*pi/180)/60
+LONGhj50=LONGhj49 + kt2*kdi2*do2*sin(b50*pi/180)/60     
+if p2+50<=length(unique(Turning2)) b51=Turning2(1,p2+50) 
+else b51=Emp 
+end
+LAThj51=LAThj50 + kt2*kdi2*do2*cos(b51*pi/180)/60
+LONGhj51=LONGhj50 + kt2*kdi2*do2*sin(b51*pi/180)/60     
+if p2+51<=length(unique(Turning2)) b52=Turning2(1,p2+51) 
+else b52=Emp 
+end
+LAThj52=LAThj51 + kt2*kdi2*do2*cos(b52*pi/180)/60
+LONGhj52=LONGhj51 + kt2*kdi2*do2*sin(b52*pi/180)/60     
+if p2+52<=length(unique(Turning2)) b53=Turning2(1,p2+52) 
+else b53=Emp 
+end
+LAThj53=LAThj52 + kt2*kdi2*do2*cos(b53*pi/180)/60
+LONGhj53=LONGhj52 + kt2*kdi2*do2*sin(b53*pi/180)/60     
+if p2+53<=length(unique(Turning2)) b54=Turning2(1,p2+53) 
+else b54=Emp 
+end
+LAThj54=LAThj53 + kt2*kdi2*do2*cos(b54*pi/180)/60
+LONGhj54=LONGhj53 + kt2*kdi2*do2*sin(b54*pi/180)/60     
+if p2+54<=length(unique(Turning2)) b55=Turning2(1,p2+54) 
+else b55=Emp 
+end 
+LAThj55=LAThj54 + kt2*kdi2*do2*cos(b55*pi/180)/60
+LONGhj55=LONGhj54 + kt2*kdi2*do2*sin(b55*pi/180)/60     
+if p2+55<=length(unique(Turning2)) b56=Turning2(1,p2+55) 
+else b56=Emp 
+end 
+LAThj56=LAThj55 + kt2*kdi2*do2*cos(b56*pi/180)/60
+LONGhj56=LONGhj55 + kt2*kdi2*do2*sin(b56*pi/180)/60     
+if p2+56<=length(unique(Turning2)) b57=Turning2(1,p2+56) 
+else b57=Emp 
+end 
+LAThj57=LAThj56 + kt2*kdi2*do2*cos(b57*pi/180)/60
+LONGhj57=LONGhj56 + kt2*kdi2*do2*sin(b57*pi/180)/60     
+if p2+57<=length(unique(Turning2)) b58=Turning2(1,p2+57) 
+else b58=Emp 
+end 
+LAThj58=LAThj57 + kt2*kdi2*do2*cos(b58*pi/180)/60
+LONGhj58=LONGhj57 + kt2*kdi2*do2*sin(b58*pi/180)/60     
+if p2+58<=length(unique(Turning2)) b59=Turning2(1,p2+58) 
+else b59=Emp 
+end 
+LAThj59=LAThj58 + kt2*kdi2*do2*cos(b59*pi/180)/60
+LONGhj59=LONGhj58 + kt2*kdi2*do2*sin(b59*pi/180)/60     
+if p2+59<=length(unique(Turning2)) b60=Turning2(1,p2+59) 
+else b60=Emp 
+end 
+LAThj60=LAThj59 + kt2*kdi2*do2*cos(b60*pi/180)/60
+LONGhj60=LONGhj59 + kt2*kdi2*do2*sin(b60*pi/180)/60
---
+++ zwrot3.m
@@ -0,0 +1,297 @@
+LAThk1=LATH1 + Kh3*kdt3*kt23*kt3*kdi3*do3*cos(c1*pi/180)/60
+LONGhk1=LongH1+ Kh3*kdt3*kt23*kt3*kdi3*do3*sin(c1*pi/180)/60     
+if p3+1<=length(unique(Turning3)) && Turn1>4 c2=Turning3(1,p3+1)
+else c2=Emp
+end
+LAThk2=LAThk1 + Kh3*kdt3*kt23*kt3*kdi3*do3*cos(c2*pi/180)/60
+LONGhk2=LONGhk1 + Kh3*kdt3*kt23*kt3*kdi3*do3*sin(c2*pi/180)/60     
+if p3+2<=length(unique(Turning3)) c3=Turning3(1,p3+2)
+else c3=Emp
+end
+LAThk3=LAThk2 + kt3*kdi3*do3*cos(c3*pi/180)/60
+LONGhk3=LONGhk2 + kt3*kdi3*do3*sin(c3*pi/180)/60     
+if p3+3<=length(unique(Turning3)) c4=Turning3(1,p3+3) 
+else c4=Emp; 
+end
+LAThk4=LAThk3 + kt3*kdi3*do3*cos(c4*pi/180)/60;
+LONGhk4=LONGhk3 + kt3*kdi3*do3*sin(c4*pi/180)/60     
+if  p3+4<=length(unique(Turning3)) c5=Turning3(1,p3+4) 
+else c5=Emp 
+end
+LAThk5=LAThk4 + kt3*kdi3*do3*cos(c5*pi/180)/60
+LONGhk5=LONGhk4 + kt3*kdi3*do3*sin(c5*pi/180)/60     
+if p3+5<=length(unique(Turning3)) c6=Turning3(1,p3+5) 
+else c6=Emp 
+end
+LAThk6=LAThk5 + kt3*kdi3*do3*cos(c6*pi/180)/60
+LONGhk6=LONGhk5 + kt3*kdi3*do3*sin(c6*pi/180)/60     
+if p3+6<=length(unique(Turning3)) c7=Turning3(1,p3+6) 
+else c7=Emp 
+end
+LAThk7=LAThk6 + kt3*kdi3*do3*cos(c7*pi/180)/60
+LONGhk7=LONGhk6 + kt3*kdi3*do3*sin(c7*pi/180)/60     
+if p3+7<=length(unique(Turning3)) c8=Turning3(1,p3+7) 
+else c8=Emp 
+end
+LAThk8=LAThk7 + kt3*kdi3*do3*cos(c8*pi/180)/60
+LONGhk8=LONGhk7 + kt3*kdi3*do3*sin(c8*pi/180)/60     
+if  p3+8<=length(unique(Turning3)) c9=Turning3(1,p3+8) 
+else c9=Emp 
+end
+LAThk9=LAThk8 + kt3*kdi3*do3*cos(c9*pi/180)/60
+LONGhk9=LONGhk8 + kt3*kdi3*do3*sin(c9*pi/180)/60     
+if p3+9<=length(unique(Turning3)) c10=Turning3(1,p3+9) 
+else c10=Emp 
+end
+LAThk10=LAThk9 + kt3*kdi3*do3*cos(c10*pi/180)/60
+LONGhk10=LONGhk9 + kt3*kdi3*do3*sin(c10*pi/180)/60     
+if p3+10<=length(unique(Turning3)) c11=Turning3(1,p3+10) 
+else c11=Emp 
+end
+LAThk11=LAThk10 + kt3*kdi3*do3*cos(c11*pi/180)/60
+LONGhk11=LONGhk10 + kt3*kdi3*do3*sin(c11*pi/180)/60     
+if p3+11<=length(unique(Turning3)) c12=Turning3(1,p3+11) 
+else c12=Emp 
+end
+LAThk12=LAThk11 + kt3*kdi3*do3*cos(c12*pi/180)/60
+LONGhk12=LONGhk11 + kt3*kdi3*do3*sin(c12*pi/180)/60     
+if p3+12<=length(unique(Turning3)) c13=Turning3(1,p3+12) 
+else c13=Emp 
+end
+LAThk13=LAThk12 + kt3*kdi3*do3*cos(c13*pi/180)/60
+LONGhk13=LONGhk12 + kt3*kdi3*do3*sin(c13*pi/180)/60     
+if p3+13<=length(unique(Turning3)) c14=Turning3(1,p3+13) 
+else c14=Emp 
+end
+LAThk14=LAThk13 + kt3*kdi3*do3*cos(c14*pi/180)/60
+LONGhk14=LONGhk13 + kt3*kdi3*do3*sin(c14*pi/180)/60     
+if p3+14<=length(unique(Turning3)) c15=Turning3(1,p3+14) 
+else c15=Emp 
+end
+LAThk15=LAThk14 + kt3*kdi3*do3*cos(c15*pi/180)/60
+LONGhk15=LONGhk14 + kt3*kdi3*do3*sin(c15*pi/180)/60     
+if p3+15<=length(unique(Turning3)) c16=Turning3(1,p3+15) 
+else c16=Emp 
+end
+LAThk16=LAThk15 + kt3*kdi3*do3*cos(c16*pi/180)/60
+LONGhk16=LONGhk15 + kt3*kdi3*do3*sin(c16*pi/180)/60     
+if p3+16<=length(unique(Turning3)) c17=Turning3(1,p3+16) 
+else c17=Emp 
+end
+LAThk17=LAThk16 + kt3*kdi3*do3*cos(c17*pi/180)/60
+LONGhk17=LONGhk16 + kt3*kdi3*do3*sin(c17*pi/180)/60     
+if p3+17<=length(unique(Turning3))  c18=Turning3(1,p3+17) 
+else c18=Emp 
+end
+LAThk18=LAThk17 + kt3*kdi3*do3*cos(c18*pi/180)/60
+LONGhk18=LONGhk17 + kt3*kdi3*do3*sin(c18*pi/180)/60     
+if p3+18<=length(unique(Turning3)) c19=Turning3(1,p3+18) 
+else c19=Emp 
+end
+LAThk19=LAThk18 + kt3*kdi3*do3*cos(c19*pi/180)/60
+LONGhk19=LONGhk18 + kt3*kdi3*do3*sin(c19*pi/180)/60     
+if p3+19<=length(unique(Turning3)) c20=Turning3(1,p3+19) 
+else c20=Emp 
+end
+LAThk20=LAThk19 + kt3*kdi3*do3*cos(c20*pi/180)/60
+LONGhk20=LONGhk19 + kt3*kdi3*do3*sin(c20*pi/180)/60     
+if p3+20<=length(unique(Turning3)) c21=Turning3(1,p3+20) 
+else c21=Emp 
+end
+LAThk21=LAThk20 + kt3*kdi3*do3*cos(c21*pi/180)/60
+LONGhk21=LONGhk20 + kt3*kdi3*do3*sin(c21*pi/180)/60     
+if p3+21<=length(unique(Turning3)) c22=Turning3(1,p3+21) 
+else c22=Emp 
+end
+LAThk22=LAThk21 + kt3*kdi3*do3*cos(c22*pi/180)/60
+LONGhk22=LONGhk21 + kt3*kdi3*do3*sin(c22*pi/180)/60     
+if p3+22<=length(unique(Turning3)) c23=Turning3(1,p3+22) 
+else c23=Emp 
+end
+LAThk23=LAThk22 + kt3*kdi3*do3*cos(c23*pi/180)/60
+LONGhk23=LONGhk22 + kt3*kdi3*do3*sin(c23*pi/180)/60     
+if p3+23<=length(unique(Turning3)) c24=Turning3(1,p3+23) 
+else c24=Emp 
+end
+LAThk24=LAThk23 + kt3*kdi3*do3*cos(c24*pi/180)/60
+LONGhk24=LONGhk23 + kt3*kdi3*do3*sin(c24*pi/180)/60     
+if p3+24<=length(unique(Turning3)) c25=Turning3(1,p3+24) 
+else c25=Emp 
+end 
+LAThk25=LAThk24 + kt3*kdi3*do3*cos(c25*pi/180)/60
+LONGhk25=LONGhk24 + kt3*kdi3*do3*sin(c25*pi/180)/60     
+if p3+25<=length(unique(Turning3)) c26=Turning3(1,p3+25) 
+else c26=Emp 
+end 
+LAThk26=LAThk25 + kt3*kdi3*do3*cos(c26*pi/180)/60
+LONGhk26=LONGhk25 + kt3*kdi3*do3*sin(c26*pi/180)/60     
+if p3+26<=length(unique(Turning3)) c27=Turning3(1,p3+26) 
+else c27=Emp 
+end 
+LAThk27=LAThk26 + kt3*kdi3*do3*cos(c27*pi/180)/60
+LONGhk27=LONGhk26 + kt3*kdi3*do3*sin(c27*pi/180)/60     
+if p3+27<=length(unique(Turning3)) c28=Turning3(1,p3+27) 
+else c28=Emp 
+end 
+LAThk28=LAThk27 + kt3*kdi3*do3*cos(c28*pi/180)/60
+LONGhk28=LONGhk27 + kt3*kdi3*do3*sin(c28*pi/180)/60     
+if p3+28<=length(unique(Turning3)) c29=Turning3(1,p3+28) 
+else c29=Emp 
+end 
+LAThk29=LAThk28 + kt3*kdi3*do3*cos(c29*pi/180)/60
+LONGhk29=LONGhk28 + kt3*kdi3*do3*sin(c29*pi/180)/60     
+if p3+29<=length(unique(Turning3)) c30=Turning3(1,p3+29) 
+else c30=Emp 
+end 
+LAThk30=LAThk29 + kt3*kdi3*do3*cos(c30*pi/180)/60
+LONGhk30=LONGhk29 + kt3*kdi3*do3*sin(c30*pi/180)/60     
+if p3+30<=length(unique(Turning3)) c31=Turning3(1,p3+30) 
+else c31=Emp 
+end 
+LAThk31=LAThk30 + kt3*kdi3*do3*cos(c31*pi/180)/60
+LONGhk31=LONGhk30 + kt3*kdi3*do3*sin(c31*pi/180)/60     
+if p3+31<=length(unique(Turning3)) c32=Turning3(1,p3+31) 
+else c32=Emp 
+end 
+LAThk32=LAThk31 + kt3*kdi3*do3*cos(c32*pi/180)/60
+LONGhk32=LONGhk31 + kt3*kdi3*do3*sin(c32*pi/180)/60     
+if p3+32<=length(unique(Turning3)) c33=Turning3(1,p3+32)
+else c33=Emp
+end
+LAThk33=LAThk32 + kt3*kdi3*do3*cos(c33*pi/180)/60
+LONGhk33=LONGhk32 + kt3*kdi3*do3*sin(c33*pi/180)/60     
+if p3+33<=length(unique(Turning3)) c34=Turning3(1,p3+33) 
+else c34=Emp 
+end
+LAThk34=LAThk33 + kt3*kdi3*do3*cos(c34*pi/180)/60
+LONGhk34=LONGhk33 + kt3*kdi3*do3*sin(c34*pi/180)/60     
+if p3+34<=length(unique(Turning3)) c35=Turning3(1,p3+34) 
+else c35=Emp 
+end
+LAThk35=LAThk34 + kt3*kdi3*do3*cos(c35*pi/180)/60
+LONGhk35=LONGhk34 + kt3*kdi3*do3*sin(c35*pi/180)/60     
+if p3+35<=length(unique(Turning3)) c36=Turning3(1,p3+35) 
+else c36=Emp 
+end
+LAThk36=LAThk35 + kt3*kdi3*do3*cos(c36*pi/180)/60
+LONGhk36=LONGhk35 + kt3*kdi3*do3*sin(c36*pi/180)/60     
+if p3+36<=length(unique(Turning3)) c37=Turning3(1,p3+36) 
+else c37=Emp 
+end
+LAThk37=LAThk36 + kt3*kdi3*do3*cos(c37*pi/180)/60
+LONGhk37=LONGhk36 + kt3*kdi3*do3*sin(c37*pi/180)/60     
+if p3+37<=length(unique(Turning3)) c38=Turning3(1,p3+37) 
+else c38=Emp 
+end
+LAThk38=LAThk37 + kt3*kdi3*do3*cos(c38*pi/180)/60
+LONGhk38=LONGhk37 + kt3*kdi3*do3*sin(c38*pi/180)/60     
+if p3+38<=length(unique(Turning3)) c39=Turning3(1,p3+38) 
+else c39=Emp 
+end
+LAThk39=LAThk38 + kt3*kdi3*do3*cos(c39*pi/180)/60
+LONGhk39=LONGhk38 + kt3*kdi3*do3*sin(c39*pi/180)/60     
+if p3+39<=length(unique(Turning3)) c40=Turning3(1,p3+39) 
+else c40=Emp 
+end
+LAThk40=LAThk39 + kt3*kdi3*do3*cos(c40*pi/180)/60
+LONGhk40=LONGhk39 + kt3*kdi3*do3*sin(c40*pi/180)/60     
+if p3+40<=length(unique(Turning3)) c41=Turning3(1,p3+40) 
+else c41=Emp 
+end
+LAThk41=LAThk40 + kt3*kdi3*do3*cos(c41*pi/180)/60
+LONGhk41=LONGhk40 + kt3*kdi3*do3*sin(c41*pi/180)/60     
+if p3+41<=length(unique(Turning3)) c42=Turning3(1,p3+41) 
+else c42=Emp 
+end
+LAThk42=LAThk41 + kt3*kdi3*do3*cos(c42*pi/180)/60
+LONGhk42=LONGhk41 + kt3*kdi3*do3*sin(c42*pi/180)/60     
+if p3+42<=length(unique(Turning3)) c43=Turning3(1,p3+42) 
+else c43=Emp 
+end
+LAThk43=LAThk42 + kt3*kdi3*do3*cos(c43*pi/180)/60
+LONGhk43=LONGhk42 + kt3*kdi3*do3*sin(c43*pi/180)/60     
+if p3+43<=length(unique(Turning3)) c44=Turning3(1,p3+43) 
+else c44=Emp 
+end
+LAThk44=LAThk43 + kt3*kdi3*do3*cos(c44*pi/180)/60
+LONGhk44=LONGhk43 + kt3*kdi3*do3*sin(c44*pi/180)/60     
+if p3+44<=length(unique(Turning3)) c45=Turning3(1,p3+44) 
+else c45=Emp 
+end
+LAThk45=LAThk44 + kt3*kdi3*do3*cos(c45*pi/180)/60
+LONGhk45=LONGhk44 + kt3*kdi3*do3*sin(c45*pi/180)/60     
+if p3+45<=length(unique(Turning3)) c46=Turning3(1,p3+45) 
+else c46=Emp 
+end
+LAThk46=LAThk45 + kt3*kdi3*do3*cos(c46*pi/180)/60
+LONGhk46=LONGhk45 + kt3*kdi3*do3*sin(c46*pi/180)/60     
+if p3+46<=length(unique(Turning3)) c47=Turning3(1,p3+46) 
+else c47=Emp 
+end
+LAThk47=LAThk46 + kt3*kdi3*do3*cos(c47*pi/180)/60
+LONGhk47=LONGhk46 + kt3*kdi3*do3*sin(c47*pi/180)/60     
+if p3+47<=length(unique(Turning3)) c48=Turning3(1,p3+47) 
+else c48=Emp 
+end
+LAThk48=LAThk47 + kt3*kdi3*do3*cos(c48*pi/180)/60
+LONGhk48=LONGhk47 + kt3*kdi3*do3*sin(c48*pi/180)/60     
+if p3+48<=length(unique(Turning3)) c49=Turning3(1,p3+48) 
+else c49=Emp 
+end
+LAThk49=LAThk48 + kt3*kdi3*do3*cos(c49*pi/180)/60
+LONGhk49=LONGhk48 + kt3*kdi3*do3*sin(c49*pi/180)/60     
+if p3+49<=length(unique(Turning3)) c50=Turning3(1,p3+49) 
+else c50=Emp 
+end
+LAThk50=LAThk49 + kt3*kdi3*do3*cos(c50*pi/180)/60
+LONGhk50=LONGhk49 + kt3*kdi3*do3*sin(c50*pi/180)/60     
+if p3+50<=length(unique(Turning3)) c51=Turning3(1,p3+50) 
+else c51=Emp 
+end
+LAThk51=LAThk50 + kt3*kdi3*do3*cos(c51*pi/180)/60
+LONGhk51=LONGhk50 + kt3*kdi3*do3*sin(c51*pi/180)/60     
+if p3+51<=length(unique(Turning3)) c52=Turning3(1,p3+51) 
+else c52=Emp 
+end
+LAThk52=LAThk51 + kt3*kdi3*do3*cos(c52*pi/180)/60
+LONGhk52=LONGhk51 + kt3*kdi3*do3*sin(c52*pi/180)/60     
+if p3+52<=length(unique(Turning3)) c53=Turning3(1,p3+52) 
+else c53=Emp 
+end
+LAThk53=LAThk52 + kt3*kdi3*do3*cos(c53*pi/180)/60
+LONGhk53=LONGhk52 + kt3*kdi3*do3*sin(c53*pi/180)/60     
+if p3+53<=length(unique(Turning3)) c54=Turning3(1,p3+53) 
+else c54=Emp 
+end
+LAThk54=LAThk53 + kt3*kdi3*do3*cos(c54*pi/180)/60
+LONGhk54=LONGhk53 + kt3*kdi3*do3*sin(c54*pi/180)/60     
+if p3+54<=length(unique(Turning3)) c55=Turning3(1,p3+54) 
+else c55=Emp 
+end 
+LAThk55=LAThk54 + kt3*kdi3*do3*cos(c55*pi/180)/60
+LONGhk55=LONGhk54 + kt3*kdi3*do3*sin(c55*pi/180)/60     
+if p3+55<=length(unique(Turning3)) c56=Turning3(1,p3+55) 
+else c56=Emp 
+end 
+LAThk56=LAThk55 + kt3*kdi3*do3*cos(c56*pi/180)/60
+LONGhk56=LONGhk55 + kt3*kdi3*do3*sin(c56*pi/180)/60     
+if p3+56<=length(unique(Turning3)) c57=Turning3(1,p3+56) 
+else c57=Emp 
+end 
+LAThk57=LAThk56 + kt3*kdi3*do3*cos(c57*pi/180)/60
+LONGhk57=LONGhk56 + kt3*kdi3*do3*sin(c57*pi/180)/60     
+if p3+57<=length(unique(Turning3)) c58=Turning3(1,p3+57) 
+else c58=Emp 
+end 
+LAThk58=LAThk57 + kt3*kdi3*do3*cos(c58*pi/180)/60
+LONGhk58=LONGhk57 + kt3*kdi3*do3*sin(c58*pi/180)/60     
+if p3+58<=length(unique(Turning3)) c59=Turning3(1,p3+58) 
+else c59=Emp 
+end 
+LAThk59=LAThk58 + kt3*kdi3*do3*cos(c59*pi/180)/60
+LONGhk59=LONGhk58 + kt3*kdi3*do3*sin(c59*pi/180)/60     
+if p3+59<=length(unique(Turning3)) c60=Turning3(1,p3+59) 
+else c60=Emp 
+end 
+LAThk60=LAThk59 + kt3*kdi3*do3*cos(c60*pi/180)/60
+LONGhk60=LONGhk59 + kt3*kdi3*do3*sin(c60*pi/180)/60
